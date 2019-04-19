569389cc3aa116836c146b932276b78a0672ab8e
Alexander <alex85desanvi@gmail.com>
2019-04-19 10:46:59 -0300

    arreglado problema con entity

--- Control_de_Inventario/Models/DB.Designer.cs
+++ Control_de_Inventario/Models/DB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Control_de_Inventario\Control_de_Inventario\Control_de_Inventario\Models\DB.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Control_de_Inventario\Control_de_Inventario\Models\DB.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
