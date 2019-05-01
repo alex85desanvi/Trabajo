494e6a934b76ad42e4b6ca7377c931b312f203b4
Alexander <alex85desanvi@gmail.com>
2019-05-01 19:07:20 -0300

    Se realizo la carga de las ventas a la base de datos.

--- Control_de_Inventario/Controllers/VentasController.cs
+++ Control_de_Inventario/Controllers/VentasController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Control_de_Inventario.Models;
 
 namespace Control_de_Inventario.Controllers
 {
@@ -13,6 +14,44 @@ namespace Control_de_Inventario.Controllers
         public JsonResult GuardarVenta(List<ItemVentaViewModel> items)
         {
             //TODO: implementar guardado de la venta
+            using (Control_de_InventarioEntities db = new Control_de_InventarioEntities())
+            {
+                decimal Total = 0;
+                int idProducto = 0;
+                int cantidad = 0;
+                decimal SubTotal = 0;
+                decimal precioUni = 0;
+
+                for(int i = 0; i < items.Count(); i++)
+                {
+                    idProducto = Convert.ToInt32(items[i].IdProducto);
+                    var produto = db.Articulo.Find(idProducto);
+                    cantidad = items[i].Cantidad;
+
+                    precioUni = produto.art_precio_uni.Value;
+
+                    SubTotal = precioUni * cantidad;
+
+                    Total = SubTotal + Total;
+                }
+                
+                db.spAgregarVenta(1, Total);
+
+                var idVenta = db.Venta.Max(x => (int?)x.id_venta) ?? 0;
+
+                
+                for (int i = 0; i < items.Count(); i++)
+                {
+                    idProducto = Convert.ToInt32(items[i].IdProducto);
+                    var produto = db.Articulo.Find(idProducto);
+                    cantidad = items[i].Cantidad;
+                    precioUni = produto.art_precio_uni.Value;
+                    db.spAgregarDetalleVenta(idVenta, idProducto, precioUni, cantidad);
+                }
+
+
+
+            }
             return Json(new { });
         }
     }
